fbc31c556442d7e8ec6e7cad995d3b0c49b663fe
fktz94 <106633973+fktz94@users.noreply.github.com>
2023-01-22 17:55:54 -0300

    listado de pokemones
    se terminó de hacer las funciones para que se listen todos los pokemones, pasando de página, haciendo un fetch sólo de ser necesario

--- src/pokedex.js
+++ src/pokedex.js
@@ -6,46 +6,86 @@ const botonIrPrincipio = $("#ir-principio");
 const botonIrFinal = $("#ir-final");
 
 function listarPokemones(url) {
+  moverse = {
+    avanzar: Boolean,
+    retroceder: Boolean,
+  };
+
   fetch(url)
     .then((respuesta) => respuesta.json())
     .then((respuesta) => {
-      console.log(respuesta);
       const pokemonesListados = $("#grilla-de-pokemones a");
       if (pokemonesListados.length > 0) pokemonesListados.remove();
       respuesta.results.forEach((pokemon) => {
         let indice = pokemon.url.split("/")[6];
         indice < 9 ? (indice = `0${indice}`) : indice;
         $("#grilla-de-pokemones").append(
-          `<a class="pl-6 py-4 bg-slate-100/30 rounded-full shadow shadow-black" id="${
+          `<a class="px-6 py-4 bg-slate-100/30 rounded-full shadow shadow-black" id="${
             pokemon.name
           }"><strong>${indice}) </strong>${pokemon.name.toUpperCase()}</a>`
         );
       });
 
-      if (respuesta.next === null) {
-        botonIrAdelante.prop("disabled", true);
+      botonIrAtras[0].href = respuesta.previous;
+      botonIrAdelante[0].href = respuesta.next;
+
+      if (botonIrAtras[0].href === null) {
+        botonIrAtras.addClass("disabled");
+        botonIrPrincipio.addClass("disabled");
       } else {
-        botonIrAdelante[0].href = respuesta.next;
+        botonIrAtras.removeClass("disabled");
+        botonIrPrincipio.removeClass("disabled");
       }
-      if (respuesta.previous === null) {
-        botonIrAtras.prop("disabled", true);
+
+      if (botonIrAdelante[0].href === null) {
+        botonIrAdelante.addClass("disabled");
+        botonIrFinal.addClass("disabled");
       } else {
-        botonIrAtras[0].href = respuesta.previous;
+        botonIrAdelante.removeClass("disabled");
+        botonIrFinal.removeClass("disabled");
       }
     });
 }
 
 listarPokemones(LINK);
 
+const irPrincipio = () => {
+  if (botonIrAtras[0].href === null) {
+    botonIrAtras.on("click", () => "");
+  } else {
+    listarPokemones(LINK);
+  }
+};
+
 function generarUrlAtras() {
-  listarPokemones(botonIrAtras[0].href);
-}
-function generarUrlAdelante() {
-  listarPokemones(botonIrAdelante[0].href);
+  if (botonIrAtras[0].href === null) {
+    botonIrAtras.on("click", () => "");
+    botonIrAtras.addClass("disabled");
+  } else {
+    listarPokemones(botonIrAtras[0].href);
+  }
 }
 
-botonIrAdelante.on("click", generarUrlAdelante);
+function generarUrlAdelante() {
+  if (botonIrAdelante[0].href === null) {
+    botonIrAdelante.on("click", () => "");
+  } else {
+    listarPokemones(botonIrAdelante[0].href);
+  }
+}
+
+const irFinal = () => {
+  if (botonIrAdelante[0].href === null) {
+    botonIrFinal.on("click", () => "");
+  } else {
+    listarPokemones(`${LINK}?limit=19&offset=1260`);
+  }
+};
+
+botonIrPrincipio.on("click", irPrincipio);
 botonIrAtras.on("click", generarUrlAtras);
+botonIrAdelante.on("click", generarUrlAdelante);
+botonIrFinal.on("click", irFinal);
 
 // Para derivar a cada pokemon ...
 const showEventTarget = (e) => {
