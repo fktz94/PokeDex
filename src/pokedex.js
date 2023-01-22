b60dae87b0f17cfc133f13f6db8fa18a7bcc28e1
fktz94 <106633973+fktz94@users.noreply.github.com>
2023-01-22 19:22:38 -0300

    enviar fetch a cada pokemon
    se logró la función para traer la información de cada pokemon en particular

--- src/pokedex.js
+++ src/pokedex.js
@@ -6,11 +6,6 @@ const botonIrPrincipio = $("#ir-principio");
 const botonIrFinal = $("#ir-final");
 
 function listarPokemones(url) {
-  moverse = {
-    avanzar: Boolean,
-    retroceder: Boolean,
-  };
-
   fetch(url)
     .then((respuesta) => respuesta.json())
     .then((respuesta) => {
@@ -18,80 +13,91 @@ function listarPokemones(url) {
       if (pokemonesListados.length > 0) pokemonesListados.remove();
       respuesta.results.forEach((pokemon) => {
         let indice = pokemon.url.split("/")[6];
-        indice < 9 ? (indice = `0${indice}`) : indice;
         $("#grilla-de-pokemones").append(
-          `<a class="px-6 py-4 bg-slate-100/30 rounded-full shadow shadow-black" id="${
-            pokemon.name
-          }"><strong>${indice}) </strong>${pokemon.name.toUpperCase()}</a>`
+          `<a class="pokemon px-6 py-4 rounded-full shadow bg-slate-100/30 shadow-black" id="${indice}">${
+            indice < 9 ? `0${indice}` : indice
+          }) ${pokemon.name.toUpperCase()}</a>`
         );
       });
 
+      console.log(respuesta.results);
       botonIrAtras[0].href = respuesta.previous;
       botonIrAdelante[0].href = respuesta.next;
 
-      if (botonIrAtras[0].href === null) {
-        botonIrAtras.addClass("disabled");
-        botonIrPrincipio.addClass("disabled");
-      } else {
-        botonIrAtras.removeClass("disabled");
-        botonIrPrincipio.removeClass("disabled");
-      }
+      habilitarYDeshabilitarBotones();
 
-      if (botonIrAdelante[0].href === null) {
-        botonIrAdelante.addClass("disabled");
-        botonIrFinal.addClass("disabled");
-      } else {
-        botonIrAdelante.removeClass("disabled");
-        botonIrFinal.removeClass("disabled");
-      }
+      verPokemon();
     });
 }
 
+//
+
+const verPokemon = () => {
+  $("#grilla-de-pokemones")
+    .children()
+    .each((i, pokemon) => {
+      pokemon.addEventListener("click", getId);
+    });
+};
+
+function getId(e) {
+  const id = e.target.attributes.id.value;
+  fetch(`${LINK}/${id}`)
+    .then((respuesta) => respuesta.json())
+    .then((respuesta) => console.log(respuesta));
+}
+
+//
+
 listarPokemones(LINK);
 
-const irPrincipio = () => {
-  if (botonIrAtras[0].href === null) {
-    botonIrAtras.on("click", () => "");
-  } else {
-    listarPokemones(LINK);
-  }
-};
+verPokemon();
+const irPrincipio = () =>
+  botonIrAtras[0].href === null
+    ? botonIrAtras.on("click", () => "")
+    : listarPokemones(LINK);
 
-function generarUrlAtras() {
-  if (botonIrAtras[0].href === null) {
-    botonIrAtras.on("click", () => "");
-    botonIrAtras.addClass("disabled");
-  } else {
-    listarPokemones(botonIrAtras[0].href);
-  }
-}
+const generarUrlAtras = () =>
+  botonIrAtras[0].href === null
+    ? botonIrAtras.on("click", () => "")
+    : listarPokemones(botonIrAtras[0].href);
 
-function generarUrlAdelante() {
-  if (botonIrAdelante[0].href === null) {
-    botonIrAdelante.on("click", () => "");
-  } else {
-    listarPokemones(botonIrAdelante[0].href);
-  }
-}
+const generarUrlAdelante = () =>
+  botonIrAdelante[0].href === null
+    ? botonIrAdelante.on("click", () => "")
+    : listarPokemones(botonIrAdelante[0].href);
 
-const irFinal = () => {
-  if (botonIrAdelante[0].href === null) {
-    botonIrFinal.on("click", () => "");
-  } else {
-    listarPokemones(`${LINK}?limit=19&offset=1260`);
-  }
-};
+const irFinal = () =>
+  botonIrAdelante[0].href === null
+    ? botonIrFinal.on("click", () => "")
+    : listarPokemones(`${LINK}?limit=19&offset=1260`);
 
 botonIrPrincipio.on("click", irPrincipio);
 botonIrAtras.on("click", generarUrlAtras);
 botonIrAdelante.on("click", generarUrlAdelante);
 botonIrFinal.on("click", irFinal);
 
-// Para derivar a cada pokemon ...
-const showEventTarget = (e) => {
-  console.log(e.target);
-  console.log(e);
-  console.log(e.target.attributes.href.value);
-};
+function habilitarYDeshabilitarBotones() {
+  if (botonIrAtras[0].href === null) {
+    botonIrAtras.addClass("disabled");
+    botonIrPrincipio.addClass("disabled");
+  } else {
+    botonIrAtras.removeClass("disabled");
+    botonIrPrincipio.removeClass("disabled");
+  }
 
-$("#h1").on("click", showEventTarget);
+  if (botonIrAdelante[0].href === null) {
+    botonIrAdelante.addClass("disabled");
+    botonIrFinal.addClass("disabled");
+  } else {
+    botonIrAdelante.removeClass("disabled");
+    botonIrFinal.removeClass("disabled");
+  }
+}
+
+// Derivar a cada pokemon ...
+// const showEventTarget = (e) => {
+//   console.log(e);
+//   console.log(e.target);
+//   console.log(e.target.attributes.href.value);
+// };
